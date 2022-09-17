9c702cffcd0598446736c9b9d340a766c59ddae0
Jacob Viveros <77998206+YakosWorkshop@users.noreply.github.com>
2022-09-16 19:35:47 -0500

    Adding last optional task

--- FlashcardApp/app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ FlashcardApp/app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.flashcardapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,20 +17,42 @@ class MainActivity : AppCompatActivity() {
         val flashAnswer1 = findViewById<View>(R.id.flashcard_answer1)
         val flashAnswer2 = findViewById<View>(R.id.flashcard_answer2)
         val flashAnswer3 = findViewById<View>(R.id.flashcard_answer3)
-        
+        val visibleEye = findViewById<ImageView>(R.id.toggle_choices_visibility)
+        val invisibleEye = findViewById<ImageView>(R.id.toggle_choices_invisibility)
 
-        findViewById<View>(R.id.flashcard_answer1). setOnClickListener {
+        visibleEye.setOnClickListener {
+
+            flashAnswer1.visibility = View.VISIBLE
+            flashAnswer2.visibility = View.VISIBLE
+            flashAnswer3.visibility = View.VISIBLE
+            visibleEye.visibility = View.INVISIBLE
+            invisibleEye.visibility = View.VISIBLE
+
+
+        }
+
+        invisibleEye.setOnClickListener {
+
+            flashAnswer1.visibility = View.INVISIBLE
+            flashAnswer2.visibility = View.INVISIBLE
+            flashAnswer3.visibility = View.INVISIBLE
+            visibleEye.visibility = View.VISIBLE
+            invisibleEye.visibility = View.INVISIBLE
+
+        }
+
+        flashAnswer1. setOnClickListener {
 
             flashAnswer1.setBackgroundColor(getResources().getColor(R.color.red, null))
             flashAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
 
         }
-        findViewById<View>(R.id.flashcard_answer2). setOnClickListener {
+        flashAnswer2. setOnClickListener {
 
             flashAnswer2.setBackgroundColor(getResources().getColor(R.color.red, null))
             flashAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
         }
-        findViewById<View>(R.id.flashcard_answer3). setOnClickListener {
+        flashAnswer3. setOnClickListener {
 
             flashAnswer3.setBackgroundColor(getResources().getColor(R.color.green, null))
 
